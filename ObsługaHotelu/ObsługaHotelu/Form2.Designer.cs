7b730b864c604274b908473de18e682fc990e102
Michał <147628252+Majkel054@users.noreply.github.com>
2025-04-06 10:52:08 +0200

    Add files via upload

---
+++ ObsługaHotelu/ObsługaHotelu/Form2.Designer.cs
@@ -0,0 +1,447 @@
+﻿
+namespace ObsługaHotelu
+{
+    partial class Pokoje
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pokojNr1 = new System.Windows.Forms.Label();
+            this.pokojNr2 = new System.Windows.Forms.Label();
+            this.pokojNr3 = new System.Windows.Forms.Label();
+            this.pokojNr4 = new System.Windows.Forms.Label();
+            this.pokojNr5 = new System.Windows.Forms.Label();
+            this.pokojNr6 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label16 = new System.Windows.Forms.Label();
+            this.label17 = new System.Windows.Forms.Label();
+            this.label18 = new System.Windows.Forms.Label();
+            this.label19 = new System.Windows.Forms.Label();
+            this.label20 = new System.Windows.Forms.Label();
+            this.label21 = new System.Windows.Forms.Label();
+            this.label22 = new System.Windows.Forms.Label();
+            this.label23 = new System.Windows.Forms.Label();
+            this.label24 = new System.Windows.Forms.Label();
+            this.label25 = new System.Windows.Forms.Label();
+            this.label26 = new System.Windows.Forms.Label();
+            this.label27 = new System.Windows.Forms.Label();
+            this.label28 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // pokojNr1
+            // 
+            this.pokojNr1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pokojNr1.Location = new System.Drawing.Point(32, 54);
+            this.pokojNr1.Name = "pokojNr1";
+            this.pokojNr1.Size = new System.Drawing.Size(80, 30);
+            this.pokojNr1.TabIndex = 0;
+            this.pokojNr1.Text = "Pokój nr 1";
+            this.pokojNr1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.pokojNr1.Click += new System.EventHandler(this.pokojNr1_Click);
+            // 
+            // pokojNr2
+            // 
+            this.pokojNr2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pokojNr2.Location = new System.Drawing.Point(32, 115);
+            this.pokojNr2.Name = "pokojNr2";
+            this.pokojNr2.Size = new System.Drawing.Size(80, 30);
+            this.pokojNr2.TabIndex = 1;
+            this.pokojNr2.Text = "Pokój nr 2";
+            this.pokojNr2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pokojNr3
+            // 
+            this.pokojNr3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pokojNr3.Location = new System.Drawing.Point(32, 180);
+            this.pokojNr3.Name = "pokojNr3";
+            this.pokojNr3.Size = new System.Drawing.Size(80, 30);
+            this.pokojNr3.TabIndex = 2;
+            this.pokojNr3.Text = "Pokój nr 3";
+            this.pokojNr3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pokojNr4
+            // 
+            this.pokojNr4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pokojNr4.Location = new System.Drawing.Point(32, 241);
+            this.pokojNr4.Name = "pokojNr4";
+            this.pokojNr4.Size = new System.Drawing.Size(80, 30);
+            this.pokojNr4.TabIndex = 3;
+            this.pokojNr4.Text = "Pokój nr 4";
+            this.pokojNr4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pokojNr5
+            // 
+            this.pokojNr5.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pokojNr5.Location = new System.Drawing.Point(32, 301);
+            this.pokojNr5.Name = "pokojNr5";
+            this.pokojNr5.Size = new System.Drawing.Size(80, 30);
+            this.pokojNr5.TabIndex = 4;
+            this.pokojNr5.Text = "Pokój nr 5";
+            this.pokojNr5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pokojNr6
+            // 
+            this.pokojNr6.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pokojNr6.Location = new System.Drawing.Point(32, 367);
+            this.pokojNr6.Name = "pokojNr6";
+            this.pokojNr6.Size = new System.Drawing.Size(80, 30);
+            this.pokojNr6.TabIndex = 5;
+            this.pokojNr6.Text = "Pokój nr 6";
+            this.pokojNr6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Location = new System.Drawing.Point(174, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 17);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Ilość miejsc";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label2.Location = new System.Drawing.Point(294, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 17);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Piętro";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label3.Location = new System.Drawing.Point(392, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(95, 17);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Łóżka podwójne";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label4.Location = new System.Drawing.Point(545, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(102, 17);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Łózka pojedyncze";
+            // 
+            // label5
+            // 
+            this.label5.Location = new System.Drawing.Point(174, 54);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(70, 30);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "3";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.Location = new System.Drawing.Point(174, 115);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(70, 30);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "3";
+            // 
+            // label7
+            // 
+            this.label7.Location = new System.Drawing.Point(174, 180);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(70, 30);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "4";
+            // 
+            // label8
+            // 
+            this.label8.Location = new System.Drawing.Point(174, 241);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(70, 30);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "4";
+            this.label8.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // label9
+            // 
+            this.label9.Location = new System.Drawing.Point(174, 301);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(70, 30);
+            this.label9.TabIndex = 14;
+            this.label9.Text = "4";
+            // 
+            // label10
+            // 
+            this.label10.Location = new System.Drawing.Point(174, 367);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(70, 30);
+            this.label10.TabIndex = 15;
+            this.label10.Text = "5";
+            // 
+            // label11
+            // 
+            this.label11.Location = new System.Drawing.Point(294, 367);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(70, 30);
+            this.label11.TabIndex = 21;
+            this.label11.Text = "2 Piętro";
+            // 
+            // label12
+            // 
+            this.label12.Location = new System.Drawing.Point(294, 301);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(70, 30);
+            this.label12.TabIndex = 20;
+            this.label12.Text = "2 Piętro";
+            // 
+            // label13
+            // 
+            this.label13.Location = new System.Drawing.Point(294, 241);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(70, 30);
+            this.label13.TabIndex = 19;
+            this.label13.Text = "1 Piętro";
+            this.label13.Click += new System.EventHandler(this.label13_Click);
+            // 
+            // label14
+            // 
+            this.label14.Location = new System.Drawing.Point(294, 180);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(70, 30);
+            this.label14.TabIndex = 18;
+            this.label14.Text = "1 Piętro";
+            // 
+            // label15
+            // 
+            this.label15.Location = new System.Drawing.Point(294, 115);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(70, 30);
+            this.label15.TabIndex = 17;
+            this.label15.Text = "Parter";
+            // 
+            // label16
+            // 
+            this.label16.Location = new System.Drawing.Point(294, 54);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(70, 30);
+            this.label16.TabIndex = 16;
+            this.label16.Text = "Parter";
+            // 
+            // label17
+            // 
+            this.label17.Location = new System.Drawing.Point(392, 367);
+            this.label17.Name = "label17";
+            this.label17.Size = new System.Drawing.Size(70, 30);
+            this.label17.TabIndex = 27;
+            this.label17.Text = "x 2";
+            // 
+            // label18
+            // 
+            this.label18.Location = new System.Drawing.Point(392, 301);
+            this.label18.Name = "label18";
+            this.label18.Size = new System.Drawing.Size(70, 30);
+            this.label18.TabIndex = 26;
+            this.label18.Text = "x 2";
+            // 
+            // label19
+            // 
+            this.label19.Location = new System.Drawing.Point(392, 241);
+            this.label19.Name = "label19";
+            this.label19.Size = new System.Drawing.Size(70, 30);
+            this.label19.TabIndex = 25;
+            this.label19.Text = "x 1";
+            // 
+            // label20
+            // 
+            this.label20.Location = new System.Drawing.Point(392, 180);
+            this.label20.Name = "label20";
+            this.label20.Size = new System.Drawing.Size(70, 30);
+            this.label20.TabIndex = 24;
+            this.label20.Text = "x 2";
+            // 
+            // label21
+            // 
+            this.label21.Location = new System.Drawing.Point(392, 115);
+            this.label21.Name = "label21";
+            this.label21.Size = new System.Drawing.Size(70, 30);
+            this.label21.TabIndex = 23;
+            this.label21.Text = "x 2";
+            // 
+            // label22
+            // 
+            this.label22.Location = new System.Drawing.Point(392, 54);
+            this.label22.Name = "label22";
+            this.label22.Size = new System.Drawing.Size(70, 30);
+            this.label22.TabIndex = 22;
+            this.label22.Text = "x 2";
+            // 
+            // label23
+            // 
+            this.label23.Location = new System.Drawing.Point(545, 367);
+            this.label23.Name = "label23";
+            this.label23.Size = new System.Drawing.Size(70, 30);
+            this.label23.TabIndex = 33;
+            this.label23.Text = "x 1";
+            // 
+            // label24
+            // 
+            this.label24.Location = new System.Drawing.Point(545, 301);
+            this.label24.Name = "label24";
+            this.label24.Size = new System.Drawing.Size(70, 30);
+            this.label24.TabIndex = 32;
+            this.label24.Text = "--";
+            // 
+            // label25
+            // 
+            this.label25.Location = new System.Drawing.Point(545, 241);
+            this.label25.Name = "label25";
+            this.label25.Size = new System.Drawing.Size(70, 30);
+            this.label25.TabIndex = 31;
+            this.label25.Text = "x 2";
+            // 
+            // label26
+            // 
+            this.label26.Location = new System.Drawing.Point(545, 180);
+            this.label26.Name = "label26";
+            this.label26.Size = new System.Drawing.Size(70, 30);
+            this.label26.TabIndex = 30;
+            this.label26.Text = "--";
+            // 
+            // label27
+            // 
+            this.label27.Location = new System.Drawing.Point(545, 115);
+            this.label27.Name = "label27";
+            this.label27.Size = new System.Drawing.Size(70, 30);
+            this.label27.TabIndex = 29;
+            this.label27.Text = "x 1";
+            // 
+            // label28
+            // 
+            this.label28.Location = new System.Drawing.Point(545, 54);
+            this.label28.Name = "label28";
+            this.label28.Size = new System.Drawing.Size(70, 30);
+            this.label28.TabIndex = 28;
+            this.label28.Text = "x 1";
+            // 
+            // Pokoje
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(724, 423);
+            this.Controls.Add(this.label23);
+            this.Controls.Add(this.label24);
+            this.Controls.Add(this.label25);
+            this.Controls.Add(this.label26);
+            this.Controls.Add(this.label27);
+            this.Controls.Add(this.label28);
+            this.Controls.Add(this.label17);
+            this.Controls.Add(this.label18);
+            this.Controls.Add(this.label19);
+            this.Controls.Add(this.label20);
+            this.Controls.Add(this.label21);
+            this.Controls.Add(this.label22);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.label16);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pokojNr6);
+            this.Controls.Add(this.pokojNr5);
+            this.Controls.Add(this.pokojNr4);
+            this.Controls.Add(this.pokojNr3);
+            this.Controls.Add(this.pokojNr2);
+            this.Controls.Add(this.pokojNr1);
+            this.Name = "Pokoje";
+            this.Text = "Pokoje";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label pokojNr1;
+        private System.Windows.Forms.Label pokojNr2;
+        private System.Windows.Forms.Label pokojNr3;
+        private System.Windows.Forms.Label pokojNr4;
+        private System.Windows.Forms.Label pokojNr5;
+        private System.Windows.Forms.Label pokojNr6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.Label label17;
+        private System.Windows.Forms.Label label18;
+        private System.Windows.Forms.Label label19;
+        private System.Windows.Forms.Label label20;
+        private System.Windows.Forms.Label label21;
+        private System.Windows.Forms.Label label22;
+        private System.Windows.Forms.Label label23;
+        private System.Windows.Forms.Label label24;
+        private System.Windows.Forms.Label label25;
+        private System.Windows.Forms.Label label26;
+        private System.Windows.Forms.Label label27;
+        private System.Windows.Forms.Label label28;
+    }
+}
