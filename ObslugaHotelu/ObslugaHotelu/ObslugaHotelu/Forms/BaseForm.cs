d861f042c5a9246de69b3665e2f6efb7b7ac739e
Michał <147628252+Majkel054@users.noreply.github.com>
2025-06-29 09:43:30 +0200

    Add files via upload

---
+++ ObslugaHotelu/ObslugaHotelu/ObslugaHotelu/Forms/BaseForm.cs
@@ -0,0 +1,17 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace ObslugaHotelu
+{
+    public class BaseForm : Form
+    {
+        public BaseForm()
+        {
+            this.StartPosition = FormStartPosition.CenterScreen;
+            this.BackColor = Color.WhiteSmoke;
+            this.Font = new Font("Segoe UI", 10);
+            this.FormBorderStyle = FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+        }
+    }
+}
